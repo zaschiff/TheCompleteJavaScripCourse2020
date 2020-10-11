4b3ccd3d14c6661e6eef40e02e839eaa119e2e33
Zach Schiff <zaschiff@gmail.com>
2020-10-11 11:21:13 -0600

    Moving to linux machine

--- Section 8 - Modern JavaScript/starter/src/js/index.js
+++ Section 8 - Modern JavaScript/starter/src/js/index.js
@@ -1,6 +1,35 @@
 // Global app controller
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
+/*
+* - Search object
+* - current recipe object
+* - Shopping list object
+* - Liked Recipes
+*/
+
+const state = {};
+
+const controlSearch = () => {
+    // 1. Get the query from the view
+    // TODO
+    const query = 'pizza';
+
+    // 2. create a new search object
+    if (query) {
+        // new search object
+        state.search = new Search(query);
+
+        // 3. Prepare the UI
+
+        // 4. search for recipes
+        state.search.getResults();
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 search.getResults();
